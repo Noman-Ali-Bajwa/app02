97e1592e0e01c276bd91c4ce50782583392d2bdf
nmn1333 <45435723+nmn1333@users.noreply.github.com>
2020-06-08 23:46:32 +0500

    git CLI commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World from a Rookie Dev</h1>
+      <p>This is the assignment for the 2nd class of the bootcamp</p>
       </div>
   );
 }
